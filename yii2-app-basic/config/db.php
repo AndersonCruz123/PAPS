60795cdf6bf705074d5fd4b6e1095abf2bbee82f
Anderson Cruz <aac@icomp.ufam.edu.br>
2015-10-31 19:09:58 -0400

    Login ok

--- yii2-app-basic/config/db.php
+++ yii2-app-basic/config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=sosufam',
     'username' => 'root',
-    'password' => '36392274',
+    'password' => '',
     'charset' => 'utf8',
     
 ];
